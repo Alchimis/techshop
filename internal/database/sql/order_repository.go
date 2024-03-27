1c05ec6df81b9fe0248643babb8990f458cd0a14
Alchimis <79978300+Alchimis@users.noreply.github.com>
2024-03-27 15:14:35 +0300

    Добавил реализацию метода получния заказов

--- internal/database/sql/order_repository.go
+++ internal/database/sql/order_repository.go
@@ -2,6 +2,9 @@ package sql
 
 import (
 	"context"
+	"encoding/json"
+
+	errs "errors"
 
 	"github.com/Alchimis/techshop/internal/errors"
 	"github.com/Alchimis/techshop/internal/models"
@@ -22,5 +25,60 @@ func (r orderRepository) GetOrdersById(ctx context.Context, ids []int) ([]models
 }
 
 func (r orderRepository) GetOrdersByIdSortByRacks(ctx context.Context, ids []int) ([]models.RackWithProducts, error) {
-	return []models.RackWithProducts{}, errors.ErrNotImplemented
+	query := `
+	SELECT 
+	p.main_rack_id,
+	p.main_rack_name,
+	json_agg(
+		json_build_object(
+			'order_id', order_has_product.order_id,
+			'product_id',p.id, 
+			'product_title',p.title, 
+			'order_quantity',order_has_product.quantity,   
+			'additional_racks',p.racks)) products
+FROM client_order
+LEFT JOIN order_has_product 
+	ON client_order.id=order_has_product.order_id
+LEFT JOIN (
+	SELECT pr.id, 
+		pr.title, 
+		pr.main_rack_id,
+		pr.main_rack_name,
+		array_agg(json_build_object('rack_id', rack.id, 'rack_name', rack.name)) racks 
+	FROM 
+		(SELECT product.id, product.title, product.main_rack_id, rack.name as main_rack_name  FROM product
+		 LEFT JOIN rack ON product.main_rack_id=rack.id
+		) pr
+	LEFT JOIN rack_has_product 
+		ON rack_has_product.product_id=pr.id 
+	LEFT JOIN rack 
+		ON rack.id=rack_has_product.rack_id
+	GROUP BY pr.id, pr.title, pr.main_rack_id, pr.main_rack_name) p
+	ON order_has_product.product_id=p.id
+WHERE client_order.id = any ($1)
+GROUP BY p.main_rack_id, p.main_rack_name;
+	`
+	rows, err := r.conn.Query(ctx, query, ids)
+	defer rows.Close()
+	if err != nil {
+		return []models.RackWithProducts{}, errs.Join(errs.New("database sql: error with query execution: "), err)
+	}
+	var racks []models.RackWithProducts
+	for rows.Next() {
+		var (
+			rack     models.RackWithProducts
+			jsonBody *string
+		)
+		rows.Scan(&rack.Id, &rack.Name, &jsonBody)
+		if jsonBody == nil {
+			return []models.RackWithProducts{}, errs.New("json body was empty")
+		}
+
+		err := json.Unmarshal([]byte(*jsonBody), &rack.Products)
+		if err != nil {
+			return racks, err
+		}
+		racks = append(racks, rack)
+	}
+	return racks, nil
 }
--- internal/models/rack.go
+++ internal/models/rack.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Rack struct {
 	Id    int
 	Title string
@@ -7,10 +12,35 @@ type Rack struct {
 
 type RackWithProducts struct {
 	Id       int
-	Products struct {
-		Id                 int
-		OrderId            int
-		Title              string
-		AdditionalRacksIds []int
+	Name     string
+	Products []struct {
+		Id              int    `json:"product_id"`
+		OrderId         int    `json:"order_id"`
+		Quantity        int    `json:"order_quantity"`
+		Title           string `json:"product_title"`
+		AdditionalRacks []struct {
+			RackName *string `json:"rack_name"`
+			RackId   *int    `json:"rack_id"`
+		} `json:"additional_racks"`
 	}
+}
+
+func (r RackWithProducts) String() string {
+	var builder strings.Builder
+	builder.WriteString(fmt.Sprintf("===Стеллаж %s\n", r.Name))
+	for _, p := range r.Products {
+		builder.WriteString(fmt.Sprintf("%s (id=%d)\n", p.Title, p.Id))
+		builder.WriteString(fmt.Sprintf("заказ %d, %d шт\n", p.OrderId, p.Quantity))
+		var racksNames []string
+		for _, m := range p.AdditionalRacks {
+			if m.RackName != nil {
+				racksNames = append(racksNames, *m.RackName)
+			}
+		}
+		if len(racksNames) > 0 {
+			builder.WriteString(fmt.Sprintf("доп стелаж: %s\n", strings.Join(racksNames, ",")))
+		}
+		builder.WriteString("\n")
+	}
+	return builder.String()
 }
